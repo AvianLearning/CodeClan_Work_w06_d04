82257407e143f4317ff393be49f014b4e12f5d36
Douglas MacIntyre <dougmac21@talk21.com>
2020-10-15 16:41:32 +0100

    Added delete button successfully.

--- lab_events/lab_events_start/js/app.js
+++ lab_events/lab_events_start/js/app.js
@@ -5,14 +5,45 @@ document.addEventListener('DOMContentLoaded', () => {
 const form = document.querySelector("#new-item-form");
 form.addEventListener("submit", handleFormSubmit);
 
+const buttonPosition = document.querySelector("#reading-list");
+const deleteButton = document.createElement("button");
+buttonPosition.appendChild(deleteButton);
+deleteButton.textContent = "Delete";
+
+deleteButton.addEventListener("click", deleteReadingList);
+
 
 });
 
 
 const handleFormSubmit = function (event) {
   event.preventDefault();
-  console.dir(event);
-  const resultParagraph = document.querySelector("#reading-list");
-  resultParagraph.textContent = `${event.target.title.value}, ${event.target.author.value}, ${event.target.category.value}`;
+  // console.dir(event);
+
+  const bookEntry = document.createElement("ul");
+
+  const title = document.createElement("li");
+  title.textContent = `${event.target.title.value}`;
+  bookEntry.appendChild(title);
+
+  const author = document.createElement("li");
+  author.textContent = `${event.target.author.value}`;
+  bookEntry.appendChild(author);
+
+  const category = document.createElement("li");
+  category.textContent = `${event.target.category.value}`;
+  bookEntry.appendChild(category);
+
+  const readingList = document.querySelector("#reading-list");
+  readingList.appendChild(bookEntry);
+
   document.getElementById("new-item-form").reset();
+};
+
+
+const deleteReadingList = function (event) {
+  const readingList = document.querySelector("#reading-list");
+
+  const body = document.querySelector("body");
+  body.removeChild(readingList);
 };
