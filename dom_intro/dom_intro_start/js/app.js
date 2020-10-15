5256746bf3868ae193531c67a9715325152fa218
Douglas MacIntyre <dougmac21@talk21.com>
2020-10-15 11:45:48 +0100

    Intro to DOM and 5 min task.

--- dom_intro/dom_intro_start/js/app.js
+++ dom_intro/dom_intro_start/js/app.js
@@ -1,1 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const title = document.querySelector("h1");
+    title.textContent = "JS says: hey! this is pretty spicy!";
 
+    const welcomeParagraph = document.querySelector("p");
+    welcomeParagraph.textContent = "Text has been changed";
+
+    console.dir(title.textContent);
+    console.log(title.textContent);
+});
+
+
+
+
+// Use document.querySelector to access the paragraph with the ID ‘welcome-paragraph’ from the DOM.
+// What is the difference in output when you use console.log and console.dir?
