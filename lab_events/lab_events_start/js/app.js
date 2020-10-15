fdc3d860286f544874330ab658a6151c5a660775
Douglas MacIntyre <dougmac21@talk21.com>
2020-10-15 15:36:52 +0100

    form submission display works and autoclears form boxes on submission - pass

--- lab_events/lab_events_start/js/app.js
+++ lab_events/lab_events_start/js/app.js
@@ -2,8 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
 
 
-
-
+const form = document.querySelector("#new-item-form");
+form.addEventListener("submit", handleFormSubmit);
 
 
 });
+
+
+const handleFormSubmit = function (event) {
+  event.preventDefault();
+  console.dir(event);
+  const resultParagraph = document.querySelector("#reading-list");
+  resultParagraph.textContent = `${event.target.title.value}, ${event.target.author.value}, ${event.target.category.value}`;
+  document.getElementById("new-item-form").reset();
+};
